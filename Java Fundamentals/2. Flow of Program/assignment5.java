5f45508e989d9068640fa2bcdad1401b634e4fbb
debarghya17 <debarghya.bhowmick@yahoo.com>
2025-06-10 16:30:14 +0530

    solved assignmnet 4 and 5 of flow of program

--- Java Fundamentals/2. Flow of Program/assignment5.java
+++ Java Fundamentals/2. Flow of Program/assignment5.java
@@ -1,6 +1,30 @@
 // Keep taking numbers as inputs till the user enters ‘x’, after that print sum of all.
-import java.io.*;
-import java.util.*;
-public class assignment5 {
-    
+import java.util.Scanner;
+public class assignment5 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        while (true) 
+        {
+            System.out.print("Enter a number (or 'x' to stop): ");
+            String input = sc.next();
+
+            if (input.equalsIgnoreCase("x")) 
+            {
+                break;
+            }
+            try 
+            {
+                int num = Integer.parseInt(input);
+                sum += num;
+            } catch (NumberFormatException e) 
+            {
+                System.out.println("Invalid input. Please enter a valid number or 'x' to stop.");
+            }
+        }
+        System.out.println("Total sum: " + sum);
+        sc.close();
+    }
 }
--- Java Fundamentals/2. Flow of Program/assignmnet4.java
+++ Java Fundamentals/2. Flow of Program/assignmnet4.java
@@ -1,6 +1,31 @@
 // Take 2 numbers as inputs and find their HCF and LCM.
 import java.io.*;
 import java.util.*;
-public class assignmnet4 {
-    
+public class assignmnet4
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int a = num1, b = num2;
+        // Finding HCF using Euclidean Algorithm
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        int hcf = a;
+
+        // Finding LCM
+        int lcm = (num1 * num2) / hcf;
+
+        // Printing results
+        System.out.println("HCF: " + hcf);
+        System.out.println("LCM: " + lcm);
+        
+        sc.close();
+    }
 }
